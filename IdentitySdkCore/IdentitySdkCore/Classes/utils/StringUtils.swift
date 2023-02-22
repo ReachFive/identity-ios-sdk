b7723da3e2d46893b3a6fd9e2a904bad828058a0
François Devémy <francois.devemy@reach5.co>
2023-02-22 22:30:26 +0100

    Réimplémentation de mkString(start:sep:end:fields[(field:name:)]:) en fonction de mkString(start:sep:end:fields[String]:)

--- IdentitySdkCore/IdentitySdkCore/Classes/utils/StringUtils.swift
+++ IdentitySdkCore/IdentitySdkCore/Classes/utils/StringUtils.swift
@@ -4,28 +4,15 @@ func mkString(start: String, fields: (field: Any?, name: String)...) -> String {
     mkString(start: "\(start)(", sep: ", ", end: ")", fields: fields)
 }
 
-//TODO réimplémenter avec mkString(start:sep:end:fields:)
 func mkString(start: String, sep: String, end: String, fields: [(field: Any?, name: String)]) -> String {
     let nonNilFields = fields.compactMap { field, name in
         if let field {
-            return (field, name)
+            return "\(name): \"\(field)\""
         } else {
             return nil
         }
     }
-    var iter = nonNilFields.makeIterator()
-    var s = start
-    var first = true
-    while let next: (field: Any, name: String) = iter.next() {
-        if first {
-            first = false
-        } else {
-            s += sep
-        }
-        s += "\(next.name): \"\(next.field)\""
-    }
-    s += end
-    return s
+    return mkString(start: start, sep: sep, end: end, fields: nonNilFields)
 }
 
 func mkString(start: String, sep: String, end: String, fields: [String]) -> String {
