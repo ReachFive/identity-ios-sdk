f9f7ebf0f28b7aaf778a5730c5396def39ea2fd5
François Devémy <francois.devemy@reach5.co>
2023-05-03 18:12:17 +0200

    Correction de la LoginWKWebview et factorisation de buildAuthorizeURL

--- IdentitySdkCore/IdentitySdkCore/Classes/LoginWKWebview.swift
+++ IdentitySdkCore/IdentitySdkCore/Classes/LoginWKWebview.swift
@@ -20,48 +20,33 @@ public class LoginWKWebview: UIView {
         self.promise = promise
         self.pkce = pkce
         
-        let scope = (scope ?? reachfive.scope).joined(separator: " ")
-        var options = [
-            "client_id": reachfive.sdkConfig.clientId,
-            "redirect_uri": reachfive.sdkConfig.scheme,
-            "response_type": "code",
-            "scope": scope,
-            "code_challenge": pkce.codeChallenge,
-            "code_challenge_method": pkce.codeChallengeMethod
-        ]
-        if let state {
-            options["state"] = state
-        }
-        if let nonce {
-            options["nonce"] = nonce
-        }
-        
-        let authURL = reachfive.reachFiveApi.buildAuthorizeURL(queryParams: options)
-        webView = WKWebView(frame: .zero, configuration: WKWebViewConfiguration())
+        let rect = CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: frame.width, height: frame.height))
+        webView = WKWebView(frame: rect, configuration: WKWebViewConfiguration())
         webView.navigationDelegate = self
         addSubview(webView)
-        
-        webView.load(URLRequest(url: authURL))
+        webView.load(URLRequest(url: reachfive.buildAuthorizeURL(pkce: pkce, state: state, nonce: nonce, scope: scope)))
     }
 }
 
 extension LoginWKWebview: WKNavigationDelegate {
     public func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> ()) {
-        guard let reachfive, let promise, let pkce else {
+        guard let reachfive,
+              let promise,
+              let pkce,
+              let url = navigationAction.request.url,
+              url.scheme == reachfive.sdkConfig.baseScheme.lowercased()
+        else {
             decisionHandler(.allow)
-            print("no reachfive, promise, pkce")
             return
         }
-        if let url = navigationAction.request.url, url.scheme == reachfive.sdkConfig.baseScheme {
-            decisionHandler(.cancel)
-            let params = URLComponents(url: url, resolvingAgainstBaseURL: true)?.queryItems
-            guard let params, let code = params.first(where: { $0.name == "code" })?.value else {
-                promise.failure(.TechnicalError(reason: "No authorization code"))
-                return
-            }
-            
-            promise.completeWith(reachfive.authWithCode(code: code, pkce: pkce))
+        
+        decisionHandler(.cancel)
+        let params = URLComponents(url: url, resolvingAgainstBaseURL: true)?.queryItems
+        guard let params, let code = params.first(where: { $0.name == "code" })?.value else {
+            promise.failure(.TechnicalError(reason: "No authorization code"))
+            return
         }
-        decisionHandler(.allow)
+        
+        promise.completeWith(reachfive.authWithCode(code: code, pkce: pkce))
     }
 }
