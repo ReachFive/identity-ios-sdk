41cca9a8f268da3505872e82c9462282d05f5c14
François Devémy <francois.devemy@reach5.co>
2023-11-27 23:09:56 +0100

    Create new framework : don't add to group

---
+++ IdentitySdkWeChat/IdentitySdkWeChat/IdentitySdkWeChat.h
@@ -0,0 +1,18 @@
+//
+//  IdentitySdkWeChat.h
+//  IdentitySdkWeChat
+//
+//  Created by François Devémy on 27/11/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IdentitySdkWeChat.
+FOUNDATION_EXPORT double IdentitySdkWeChatVersionNumber;
+
+//! Project version string for IdentitySdkWeChat.
+FOUNDATION_EXPORT const unsigned char IdentitySdkWeChatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IdentitySdkWeChat/PublicHeader.h>
+
+
