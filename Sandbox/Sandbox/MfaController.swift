19d5a4e94a12e1728a6fe23fe9ee0ea9376c2a2c
sacha <sacha.memmi@reach5.co>
2024-02-07 11:27:57 +0100

    refactoring + improve UI

--- Sandbox/Sandbox/MfaController.swift
+++ Sandbox/Sandbox/MfaController.swift
@@ -2,15 +2,13 @@ import Foundation
 import UIKit
 import IdentitySdkCore
 
-class MfaCredentialsController: UIViewController {
+class MfaController: UIViewController {
     var authToken: AuthToken?
     
     var clearTokenObserver: NSObjectProtocol?
     var setTokenObserver: NSObjectProtocol?
     
     
-    @IBOutlet weak var redirectUrl: UITextField!
-    @IBOutlet weak var emailVerificationCode: UITextField!
     @IBOutlet weak var phoneNumberVerificationCode: UITextField!
     @IBOutlet weak var phoneNumberVerifyRegistration: UITextField!
     @IBOutlet weak var phoneNumberStartRegistration: UITextField!
@@ -36,11 +34,9 @@ class MfaCredentialsController: UIViewController {
     func didLogout() {
         print("ProfileController.didLogout")
         authToken = nil
-        redirectUrl.text = nil
         phoneNumberStartRegistration.text = nil
         phoneNumberVerifyRegistration.text = nil
         phoneNumberVerificationCode.text = nil
-        emailVerificationCode.text = nil
     }
     @IBAction func startMfaPhoneRegistration(_ sender: UIButton) {
         print("MfaCredentialsController.startMfaPhoneRegistration")
@@ -88,50 +84,6 @@ class MfaCredentialsController: UIViewController {
             }
             .onFailure { error in
                 let alert = AppDelegate.createAlert(title: "Verify MFA email Registration", message: "Error: \(error.message())")
-                self.present(alert, animated: true, completion: nil)
-            }
-    }
-    
-    @IBAction func verifyMfaEmailRegistration(_ sender: UIButton) {
-        print("MfaCredentialsController.verifyMfaEmailRegistration")
-        guard let authToken else {
-            print("not logged in")
-            return
-        }
-        let verificationCode = emailVerificationCode.text
-        guard let verificationCode else {
-            print("verification cannot be empty")
-            return
-        }
-        AppDelegate.reachfive()
-            .verifyMfaCredentialRegistration(authToken: authToken, request: VerifyMfaCredentialRegistrationRequest.Email(verificationCode: verificationCode))
-            .onSuccess {
-                let alert = AppDelegate.createAlert(title: "Verify MFA email Registration", message: "Success")
-                self.present(alert, animated: true, completion: nil)
-            }
-            .onFailure { error in
-                let alert = AppDelegate.createAlert(title: "Verify MFA email Registration", message: "Error: \(error.message())")
-                self.present(alert, animated: true, completion: nil)
-            }
-        
-    }
-    @IBAction func startMfaEmailRegistration(_ sender: UIButton) {
-        print("MfaCredentialsController.startMfaEmailRegistration")
-        guard let authToken else {
-            print("not logged in")
-            return
-        }
-        AppDelegate.reachfive()
-            .startMfaCredentialRegistration(
-                authToken: authToken,
-                request: StartMfaCredentialRegistrationRequest.Email(redirectUrl: redirectUrl.text)
-            )
-            .onSuccess {
-                let alert = AppDelegate.createAlert(title: "Start MFA email Registration", message: "Success")
-                self.present(alert, animated: true, completion: nil)
-            }
-            .onFailure { error in
-                let alert = AppDelegate.createAlert(title: "Start MFA email Registration", message: "Error: \(error.message())")
                 self.present(alert, animated: true, completion: nil)
             }
     }
---
+++ Sandbox/Sandbox/PasskeyCredentialController.swift
@@ -0,0 +1,9 @@
+//
+//  PasskeyCredentialController.swift
+//  Sandbox
+//
+//  Created by Sacha on 06/02/2024.
+//  Copyright © 2024 Reachfive. All rights reserved.
+//
+
+import Foundation
