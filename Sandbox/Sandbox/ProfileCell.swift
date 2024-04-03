073277c956b6fe30ab790df2bc68a143a21e9ac2
François Devémy <francois.devemy@reach5.co>
2024-04-03 16:27:19 +0200

    Ajout d'une sous-classe de cellule

---
+++ Sandbox/Sandbox/ProfileCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileCell.swift
+//  Sandbox
+//
+//  Created by François Devémy on 03/04/2024.
+//  Copyright © 2024 Reachfive. All rights reserved.
+//
+
+import UIKit
+
+class ProfileCell: UICollectionViewCell {
+    
+}
