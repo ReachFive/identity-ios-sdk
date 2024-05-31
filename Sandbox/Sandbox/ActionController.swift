73d2b45be853a9d69492ac71e4548ff201e09c4e
François Devémy <francois.devemy@reach5.co>
2024-05-31 18:04:13 +0200

    Action

--- Sandbox/Sandbox/ActionController.swift
+++ Sandbox/Sandbox/ActionController.swift
@@ -4,6 +4,29 @@ import IdentitySdkCore
 import AuthenticationServices
 
 class ActionController: UITableViewController {
+    
+    override func viewDidLoad() {
+        AppDelegate.reachfive().addAccountRecoveryCallback { result in
+            print("addAccountRecoveryCallback \(result)")
+            switch result {
+            case .success(let resp):
+                guard let window = self.view.window else { fatalError("The view was not in the app's view hierarchy!") }
+                if #available(macCatalyst 16.0, *) {
+                    AppDelegate.reachfive().resetPasskeys(withRequest: ResetPasskeyRequest(verificationCode: resp.verificationCode, friendlyName: resp.email, anchor: window, email: resp.email))
+                        .onSuccess { () in
+                            print("succcess reset")
+                        }
+                        .onFailure { error in
+                            print("Error: \(error.message())")
+                        }
+                }
+            case .failure(let error):
+                let alert = AppDelegate.createAlert(title: "Account Recovery Failed", message: "Error: \(error.message())")
+                self.present(alert, animated: true)
+            }
+        }
+    }
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         
@@ -53,6 +76,30 @@ class ActionController: UITableViewController {
                     }
             }
         }
+        
+        // Section Recovery
+        if indexPath.section == 5 {
+            // Login with refresh
+            if indexPath.row == 1 {
+                let alert = UIAlertController(title: "Recover account", message: "Enter your identifier", preferredStyle: .alert)
+                alert.addTextField()
+                alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+                let sendAction = UIAlertAction(title: "Send recovery link", style: .default) { [unowned alert] (_) in
+                    let textField = alert.textFields?[0]
+                    AppDelegate.reachfive().requestAccountRecovery(email: textField?.text)
+                        .onSuccess { () in
+                            self.showToast(message: "Email Sent", seconds: 1)
+                        }
+                        .onFailure { error in
+                            let alert = AppDelegate.createAlert(title: "Login failed", message: "Error: \(error.message())")
+                            self.present(alert, animated: true)
+                        }
+                }
+                alert.addAction(sendAction)
+                alert.preferredAction = sendAction
+                self.present(alert, animated: true)
+            }
+        }
     }
     
     override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
@@ -64,7 +111,7 @@ class ActionController: UITableViewController {
             return nil
         }
         #if targetEnvironment(macCatalyst)
-            if indexPath.section == 2, indexPath.row == 3 {
+        if indexPath.section == 2, indexPath.row == 3 {
             let alert = AppDelegate.createAlert(title: "Login", message: "AutoFill not available on macOS")
             present(alert, animated: true)
             return nil
