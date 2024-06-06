fac982f63a44955387f998b1a6e0d704047333f9
François Devémy <francois.devemy@reach5.co>
2024-06-06 18:46:39 +0200

    Ajout des dépendances

--- Package.swift
+++ Package.swift
@@ -11,11 +11,22 @@ let package = Package(
             name: "IdentitySdkCore",
             targets: ["IdentitySdkCore"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.9.1")),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMajor(from: "1.8.2")),
+        .package(url: "https://github.com/devicekit/DeviceKit.git", from: "5.1"),
+        .package(url: "https://github.com/Thomvis/BrightFutures.git", from: "8.2"),
+    
+    
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "IdentitySdkCore",
+            dependencies: [
+                .product(name: "Alamofire", package: "Alamofire")
+            ],
             path: "IdentitySdkCore/IdentitySdkCore"),
     ]
 )
