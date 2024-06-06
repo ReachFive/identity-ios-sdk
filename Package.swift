962672c1367a79ee9b3d36aa3148d762af89e290
François Devémy <francois.devemy@reach5.co>
2024-06-06 19:50:43 +0200

    Renommage en Reach5. Ajout du module Google.

--- Package.swift
+++ Package.swift
@@ -5,25 +5,24 @@ import PackageDescription
 
 let package = Package(
     name: "IdentitySdkCore",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "IdentitySdkCore",
-            targets: ["IdentitySdkCore"]),
+        .library(name: "Reach5", targets: ["Reach5"]),
+        .library(name: "Reach5Google", targets: ["Reach5Google"]),
     ],
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.9.1")),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMajor(from: "1.8.2")),
         .package(url: "https://github.com/devicekit/DeviceKit.git", .upToNextMajor(from: "5.1.0")),
         .package(url: "https://github.com/Thomvis/BrightFutures.git", .upToNextMajor(from: "8.2.0")),
-    
-    
+        .package(url: "https://github.com/google/GoogleSignIn-iOS.git", .upToNextMajor(from: "7.1.0")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "IdentitySdkCore",
+            name: "Reach5",
             dependencies: [
                 .product(name: "Alamofire", package: "Alamofire"),
                 .product(name: "CryptoSwift", package: "CryptoSwift"),
@@ -31,5 +30,9 @@ let package = Package(
                 .product(name: "BrightFutures", package: "BrightFutures"),
             ],
             path: "IdentitySdkCore/IdentitySdkCore"),
+        .target(
+            name: "Reach5Google",
+            dependencies: ["Reach5", "GoogleSignIn"],
+            path: "IdentitySdkGoogle/IdentitySdkGoogle"),
     ]
 )
