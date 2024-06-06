9ff52ef1f282b245917f8f750ab189f8cfdd98bb
François Devémy <francois.devemy@reach5.co>
2024-06-06 18:31:14 +0200

    Ajout d'un chemin

--- Package.swift
+++ Package.swift
@@ -15,9 +15,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "IdentitySdkCore"),
-        .testTarget(
-            name: "IdentitySdkCoreTests",
-            dependencies: ["IdentitySdkCore"]),
+            name: "IdentitySdkCore",
+            path: "IdentitySdkCore/IdentitySdkCore"),
     ]
 )
