e12b1f0afd1fbcd5d48c966245f4c9a7b2af9b77
François Devémy <francois.devemy@reach5.co>
2024-06-07 15:04:13 +0200

    GoogleSignIn-iOS

--- Package.swift
+++ Package.swift
@@ -34,7 +34,7 @@ let package = Package(
             name: "IdentitySdkGoogle",
             dependencies: [
                 "IdentitySdkCore",
-                .product(name: "GoogleSignIn-iOS", package: "GoogleSignIn"),
+                .product(name: "GoogleSignIn-iOS", package: "GoogleSignIn-iOS"),
             ],
             path: "IdentitySdkGoogle/IdentitySdkGoogle"),
     ]
