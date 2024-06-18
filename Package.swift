fd5eb05144e6a83535ee393963e98020890c8dd5
François Devémy <francois.devemy@reach5.co>
2024-06-18 18:49:36 +0200

    Test non concluant d'avoir deux packages

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let package = Package(
+let package1 = Package(
     name: "Reach5Facebook",
     platforms: [
         .iOS(.v13)
@@ -14,19 +14,26 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/facebook/facebook-ios-sdk.git", .upToNextMajor(from: "17.0.0")),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.9.1")),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMajor(from: "1.8.2")),
+        .package(url: "https://github.com/devicekit/DeviceKit.git", .upToNextMajor(from: "5.1.0")),
+        .package(url: "https://github.com/Thomvis/BrightFutures.git", .upToNextMajor(from: "8.2.0")),
+
+//        .package(path: "IdentitySdkCore", "1.0.0"..<"2.0.0")
+//        .package(url: "https://github.com/ReachFive/identity-ios-sdk.git", .branch())
     ],
     targets: [
         .target(
             name: "IdentitySdkFacebook",
             dependencies: [
-                "IdentitySdkCore",
+//                "IdentitySdkCore",
                 .product(name: "FacebookCore", package: "facebook-ios-sdk"),
                 .product(name: "FacebookLogin", package: "facebook-ios-sdk"),
             ],
             path: "IdentitySdkFacebook/IdentitySdkFacebook"),
     ]
 )
-let packageF = Package(
+let package2 = Package(
     name: "Reach5",
     platforms: [
         .iOS(.v13)
